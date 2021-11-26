2365ccce98247028f6ca0f9d683accb4897ed388
Kasich <pjesus192@gmail.com>
2021-11-26 00:15:08 -0300

    edicoes desafio 115a

--- Desafio115a/interface/__init__.py
+++ Desafio115a/interface/__init__.py
@@ -1,7 +1,43 @@
-def principal():
-    print('-'*30)
-    print(f'{"Menu Principal":^30}')
-    print('-'*30)
+import time
+from time import sleep
 
 
-principal()
+len_carac = 45
+
+
+def print_linha():
+    print('-' * len_carac)
+
+
+def menu_principal():
+    print_linha()
+    print(f'{"Menu Principal":^{len_carac}}')
+    print_linha()
+    print(
+        f'''{"1 - Ver pessoas cadastradas":<{len_carac}}\n{"2 - Cadastrar nova pessoa":<{len_carac}}\n{"3 - Sair do sistema":<{len_carac}}'''
+    )
+    print_linha()
+    escolha = int(input('Sua opção: '))
+    return escolha
+
+
+def main():
+    escolha = 'x'
+    while escolha not in [1, 2, 3]:
+        escolha = menu_principal()
+        try:
+            int(escolha)
+        except(ValueError, TypeError):
+            print('Digite um valor válido')
+        if escolha not in [1, 2, 3]:
+            print('Digite um valor dentro dos valores válidos do menu')
+            time.sleep(1)
+    if escolha == 1:
+
+    if escolha == 3:
+        quit()
+
+
+if __name__ == '__main__':
+    while True:
+        main()
